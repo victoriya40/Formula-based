2ae79cd75bf84438a2f75be74f9adacfd1992fe7
victoriya40 <victoriyakrishnan@gmail.com>
2025-12-11 21:38:12 +0530

    Add files via upload

---
+++ Main (32).java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+			System.out.println("a: ");
+		int a=sc.nextInt();
+		System.out.println("b: ");
+		int b=sc.nextInt();
+		System.out.println("c: ");
+		int c=sc.nextInt();
+		int result=(a+b+c);
+		System.out.println("Perimeter of the triangle: "+result);
+	}
+}
