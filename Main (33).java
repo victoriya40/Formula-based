f74e80f1b7d5a5d3350d06ac3194d23ca7447667
victoriya40 <victoriyakrishnan@gmail.com>
2025-12-11 21:59:39 +0530

    Add files via upload

---
+++ Main (33).java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+			System.out.println("Radius: ");
+		int r=sc.nextInt();
+		Double result=(2*3.14*r);
+		System.out.println("Circumference of circle: "+result);
+	}
+}
