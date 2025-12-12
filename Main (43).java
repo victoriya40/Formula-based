7089439724d0c4d2221bf8726e3d5f919189d380
victoriya40 <victoriyakrishnan@gmail.com>
2025-12-12 14:24:23 +0530

    Add files via upload

---
+++ Main (43).java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    System.out.println("Radius: ");
+	    int r=sc.nextInt();
+	    Double result1=4*3.14*r*r;
+	    Double result2=4/3*3.14*r*r*r;
+	    System.out.println("Surface area of sphere: "+result1);
+	   System.out.println("Volume of sphere: "+result2);
+}
+}
